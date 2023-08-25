735a4e46ad1099db3dba71c5df87fa5196061226
lifeafter619 <65111206+lifeafter619@users.noreply.github.com>
2023-08-25 14:28:41 +0800

    Update Hero.js

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -270,7 +270,7 @@ function TodayCard({ cRef }) {
                     <div id='more' className='select-none'>更多推荐</div>
                 </div>
             </div>
-            <div id='today-card-cover' className={`${isCoverUp ? '' : ' pointer-events-none'} cursor-pointer today-card-cover absolute w-full h-full top-0`} style={{ background: "url('https://image.66619.eu.org/file/70732099cfd68ab3ab813.png') no-repeat center /cover" }}></div>
+            <div id='today-card-cover' className={`${isCoverUp ? '' : ' pointer-events-none'} cursor-pointer today-card-cover absolute w-full h-full top-0`} style={{ background: "url('https://image.66619.eu.org/file/ea221381709f4df432a4d.jpg') no-repeat center /cover" }}></div>
         </div>
     </div>
 }
