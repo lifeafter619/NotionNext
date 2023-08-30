d43283c6eff6ad8c2c295c8269aea101b10fd9d5
lifeafter619 <65111206+lifeafter619@users.noreply.github.com>
2023-08-30 15:42:07 +0800

    Update WalineComponent.js

--- components/WalineComponent.js
+++ components/WalineComponent.js
@@ -76,8 +76,8 @@ const WalineComponent = (props) => {
         reaction: true,
         dark: 'html.dark',
         emoji: [
-          '//npm.elemecdn.com/@waline/emojis@1.2.0/qq',
           '//npm.elemecdn.com/sticker-heo@2022.7.5/Sticker-100',
+          '//npm.elemecdn.com/@waline/emojis@1.2.0/qq',
           '//npm.elemecdn.com/@waline/emojis@1.2.0/tieba',
           '//npm.elemecdn.com/@waline/emojis@1.2.0/weibo',
           '//npm.elemecdn.com/@waline/emojis@1.2.0/bilibili',
