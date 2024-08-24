0d3d8c3a76bbde7657067f59fe3f22d323b6abee
Etherrreal. <65111206+lifeafter619@users.noreply.github.com>
2024-08-24 09:25:16 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '𝟞𝟙𝟡', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '𝓙𝓾𝓼𝓽 𝓪 𝓬𝓵𝓸𝓾𝓭.', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://66619.eu.org', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '生活,技术,619, 𝟞𝟙𝟡'𝕤 𝔹𝕃𝕆𝔾,619's blog,619's Blog,博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '生活,技术,619,619's blog,619's Blog,博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'mail@66619.eu.org', // 邮箱地址 例如mail@tangly1024.com
