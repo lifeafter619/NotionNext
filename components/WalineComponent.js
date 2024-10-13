6ca68bc123372da82597d6ba6480da286e7a7fb5
Etherrreal. <65111206+lifeafter619@users.noreply.github.com>
2024-10-13 13:24:46 +0800

    Update WalineComponent.js

--- components/WalineComponent.js
+++ components/WalineComponent.js
@@ -81,8 +81,8 @@ const WalineComponent = (props) => {
           '//npm.elemecdn.com/@waline/emojis@1.2.0/tieba',
           '//npm.elemecdn.com/@waline/emojis@1.2.0/weibo',
           '//npm.elemecdn.com/@waline/emojis@1.2.0/bilibili',
-          '//file.cdn.sbbb.us.kg/beluga-emoji',
-          '//file.cdn.sbbb.us.kg/ikun-emoji'
+          '//file.sbbb.us.kg/beluga-emoji',
+          '//file.sbbb.us.kg/ikun-emoji'
         ]
       })
     }
